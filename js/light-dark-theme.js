6d3a3cceb6ab07c8d0aebbbc632d6ed1879aec26
drygalapedro <drygalapedro@gmail.com>
2023-07-27 10:42:42 -0300

    musica e tema

--- js/light-dark-theme.js
+++ js/light-dark-theme.js
@@ -1,1 +1,17 @@
 //alterar o tema para claro ou escuro
+
+const tema = document.getElementById('tema')
+const botao = document.getElementById('toggle-theme')
+const corpo = document.getElementById('corpo')
+
+botao.addEventListener('click', () => {
+    if(tema.innerText=="light_mode"){
+        tema.innerText="dark_mode"
+        corpo.setAttribute('class', 'light')
+    }
+    else{
+        tema.innerText="light_mode" 
+        corpo.setAttribute('class', 'dark')
+    }
+
+})
--- js/musica.js
+++ js/musica.js
@@ -1,1 +1,19 @@
 //reprodutor de musica
+
+const botao = document.getElementById('botao');
+const audioPlay = document.getElementById('audioPlay');
+const reprodutor = document.getElementById('reprodutor');
+
+
+
+botao.addEventListener('click', () => {
+    if (reprodutor.innerText=="play_arrow") {
+        reprodutor.innerText="pause"
+        audioPlay.play()
+        audioPlay.setAttribute('loop','true')
+    }
+    else{
+        reprodutor.innerText="play_arrow"
+        audioPlay.pause()
+    }
+});
